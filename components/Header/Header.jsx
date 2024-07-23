b5445861b13dcd901e5e9aa0b3c3c7462969482a
Alona Krupenko <alonakrupenko@Alonas-MacBook-Pro.local>
2024-07-23 15:34:59 +0200

    improved header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,8 +3,11 @@
 import Image from "next/image";
 import Logo from "./../../public/Star_Wars_Logo.svg";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const path = usePathname();
+
   return (
     <div className="bg-black mb-6">
       <div className="container mx-auto px-6 h-16 py-2 flex justify-between content-center ">
@@ -13,13 +16,16 @@ const Header = () => {
             <Image src={Logo} alt="Starwars logo" />
           </div>
         </Link>
-        <Link href={`/heroesList`}>
-          <button className="bg-yellow-300 hover:bg-yellow-400 py-2 px-4 border border-yellow-300 hover:border-yellow-400 rounded">
-            GO TO LIST
-          </button>
-        </Link>
+        {path === "/" || path === "/heroesList" ? null : (
+          <Link href={`/heroesList`}>
+            <button className="bg-yellow-300 hover:bg-yellow-400 py-2 px-4 border border-yellow-300 hover:border-yellow-400 rounded">
+              GO TO LIST
+            </button>
+          </Link>
+        )}
       </div>
     </div>
   );
 };
+
 export default Header;
